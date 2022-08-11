9dfbaa4bf18a2b82461f6f865182a3b005b1e796
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-08-11 13:08:43 +0600

    finish 8.1

--- part8-fso/index.js
+++ part8-fso/index.js
@@ -27,11 +27,6 @@ let authors = [
 ]
 
 /*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
 */
@@ -90,11 +85,15 @@ let books = [
 
 const typeDefs = gql`
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
     Query: {
+        bookCount: () => books.length,
+        authorCount: () => authors.length
     }
 }
 
