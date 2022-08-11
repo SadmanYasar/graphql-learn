bdbd7b87650654903d233461e6b5935e32627bca
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-08-12 02:53:20 +0600

    init CRA

---
+++ phonebook-frontend/src/index.js
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
+const query = gql`
+query {
+  allPersons {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+ReactDOM.render(<App />, document.getElementById('root'))
