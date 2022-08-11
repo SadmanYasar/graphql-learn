454bbeaa9576c5c03e4a5e19a98b1013d7368489
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-08-11 13:43:53 +0600

    finish 8.3

--- part8-fso/index.js
+++ part8-fso/index.js
@@ -84,16 +84,40 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
+        authorCount: () => authors.length,
+        allBooks: () => books,
+        allAuthors: () => authors
+    },
+    Author: {
+        bookCount: (root) => {
+            return books.filter(b => b.author === root.name).length
+        }
     }
 }
 
