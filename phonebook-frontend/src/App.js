3053b6942d5b1040049e5dc3ec6142b8df509ce7
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-08-14 01:19:43 +0600

    finish 8.8

---
+++ phonebook-frontend/src/App.js
@@ -0,0 +1,33 @@
+import { useQuery } from '@apollo/client'
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { ALL_AUTHORS } from './queries'
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors show={page === 'authors'} authors={result.data.allAuthors} />
+
+      <Books show={page === 'books'} />
+
+      <NewBook show={page === 'add'} />
+    </div>
+  )
+}
+
+export default App
