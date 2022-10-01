ddbc06caa31edbf59372cee99fd7b64ccf60e314
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-10-01 08:28:00 +0600

    finish 8.19

--- phonebook-frontend/src/components/Books.js
+++ phonebook-frontend/src/components/Books.js
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react"
+
 const Books = (props) => {
-  
+  const [filter, setFilter] = useState([])
+  const [genres, setGenres] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState('')
+
+  useEffect(() => {
+    const books = props.books
+    let genres = ['ALL BOOKS']
+
+    books.forEach(b => {
+      b.genres.forEach(g => {
+        if (genres.indexOf(g) === -1) {
+          genres.push(g)
+        }
+      })
+    })
+
+    setGenres(genres)
+    setSelectedGenre('ALL BOOKS')
+  }, [props.books])
+
+  useEffect(() => {
+    if (selectedGenre === 'ALL BOOKS') {
+      setFilter(props.books)
+    } else {
+      setFilter(props.books.filter(b => b.genres.indexOf(selectedGenre) !== -1))
+    }
+  }, [props.books, selectedGenre])
 
   if (!props.show) {
     return null
   }
-
-  const books = props.books
 
   return (
     <div>
@@ -18,7 +44,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filter.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -27,8 +53,14 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
-
-      
+      <div>
+        {genres.length > 0 &&
+          genres.map((g) => (
+            <button onClick={() => setSelectedGenre(g)} key={g}>
+              {g}
+            </button>
+          ))}
+      </div>
     </div>
   )
 }
